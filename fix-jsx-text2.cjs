9af6ae17e7407874af5752bac5d83e03d51d1175
XiterBug <anakselatan34@gmail.com>
2026-07-07 22:45:09 +0700

    Hapus fix-jsx-text2.cjs

--- fix-jsx-text2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-
-code = code.replace(/>Unduh</g, ">{t('Unduh')}<");
-code = code.replace(/>Sedang Memproses...</g, ">{t('Sedang Memproses...')}<");
-code = code.replace(/>Riwayat kosong</g, ">{t('Riwayat kosong')}<");
-
-fs.writeFileSync('src/App.tsx', code);
